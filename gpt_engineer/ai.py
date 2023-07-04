dd90bdaa4f5297fc7ed073e9d5d1b4b5a7d05625
karlotimmerman <karlo.timmerman@skydust.io>
2023-07-04 08:42:46 +0000

    add

--- gpt_engineer/ai.py
+++ gpt_engineer/ai.py
@@ -11,7 +11,16 @@ import tiktoken
 
 logger = logging.getLogger(__name__)
 
-set openai_api_key=os.getenv("OPENAI_API_KEY")
+import os
+
+# Retrieve the value of the OPENAI_API_KEY environment variable
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Check if the environment variable is set
+if openai_api_key is None:
+    print("The OPENAI_API_KEY environment variable is not set.")
+else:
+    print("The value of OPENAI_API_KEY is:", openai_api_key)
 
 
 @dataclass
